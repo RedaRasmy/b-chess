1733547c79dd70422ac9a8ce7277fc1288eb2ae5
reda <red7mer@gmail.com>
2026-03-05 17:52:27 +0000

    feat: add google and github login buttons

---
+++ src/features/auth/components/github-button.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button"
+import GithubIcon from "@/features/auth/components/github-icon"
+import { authClient } from "@/lib/auth-client"
+
+export default function GithubButton() {
+    async function handleClick() {
+        await authClient.signIn.social({
+            provider: "github",
+            callbackURL: "/profile",
+            errorCallbackURL: "/auth/login",
+        })
+    }
+    return (
+        <Button
+            variant="outline"
+            className="w-full cursor-pointer"
+            onClick={handleClick}
+        >
+            <GithubIcon />
+            Github
+        </Button>
+    )
+}
---
+++ src/features/auth/components/google-button.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button"
+import GoogleIcon from "@/features/auth/components/google-icon"
+import { authClient } from "@/lib/auth-client"
+
+export default function GoogleButton() {
+    async function handleClick() {
+        await authClient.signIn.social({
+            provider: "google",
+            callbackURL: "/profile",
+            errorCallbackURL: "/auth/login",
+        })
+    }
+    return (
+        <Button
+            variant="outline"
+            className="w-full cursor-pointer"
+            onClick={handleClick}
+        >
+            <GoogleIcon />
+            Google
+        </Button>
+    )
+}
