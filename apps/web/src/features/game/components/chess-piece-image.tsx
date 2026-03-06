5836ed54c702e6573f3eaf1ace707c8ac2a1fa38
reda <red7mer@gmail.com>
2026-03-06 22:49:09 +0000

    feat: add ChessPieceImage component

---
+++ apps/web/src/features/game/components/chess-piece-image.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils"
+import { Piece } from "chess.js"
+import Image from "next/image"
+
+export default function ChessPieceImage({
+    piece,
+    className,
+    size,
+}: {
+    piece: Piece
+    className?: string
+    size?: number
+}) {
+    const { color, type } = piece
+
+    return (
+        <Image
+            data-testid={`${color}${type}`}
+            alt={type}
+            width={size}
+            height={size}
+            fill={!size}
+            sizes="(max-width:50px)"
+            draggable={false}
+            src={`/images/chess-pieces/${color}${type}.png`}
+            className={cn(`select-none`, className)}
+        />
+    )
+}
