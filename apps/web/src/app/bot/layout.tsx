bae65daccc6c3c7eaef59ce466b54d8bf2cb67af
reda <red7mer@gmail.com>
2026-03-09 15:41:11 +0000

    chore: clean logs

--- apps/web/src/app/bot/layout.tsx
+++ apps/web/src/app/bot/layout.tsx
@@ -2,7 +2,6 @@
 import { ReactNode, useEffect } from "react"
 
 export default function Layout({ children }: { children: ReactNode }) {
-    console.log("hello from tempalte")
     useEffect(() => {
         fetch("/stockfish.js").catch(() => {})
         fetch("/stockfish.wasm").catch(() => {})
