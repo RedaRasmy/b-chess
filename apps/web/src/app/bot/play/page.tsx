d3c4626cdd21cf6f659d551fb698f48ac56d1b3c
reda <red7mer@gmail.com>
2026-03-10 17:54:17 +0000

    refactor: add BotControls component

--- apps/web/src/app/bot/play/page.tsx
+++ apps/web/src/app/bot/play/page.tsx
@@ -1,25 +1,15 @@
 "use client"
-import { Button } from "@/components/ui/button"
+import BotControls from "@/features/bot/components/bot-controls"
 import BotEndDialog from "@/features/bot/components/bot-end-dialog"
-import { useBotStore } from "@/features/bot/store"
 import useBot from "@/features/bot/use-bot"
 import GameBoard from "@/features/game/components/game-board"
-import HistoryController from "@/features/game/components/history-controller"
 import PlayerInfo from "@/features/game/components/player-info"
 import { useGameStore } from "@/features/game/game-store"
 import { getColor } from "@/features/game/utils/get-color"
-import { Plus, RotateCcw, Undo } from "lucide-react"
-import Link from "next/link"
 
 export default function Page() {
     useBot()
-    const { playerColor, resetGame, undo, moveHistory } = useGameStore()
-    const replay = useBotStore((s) => s.replayBotGame)
-
-    const canUndo =
-        playerColor === "white"
-            ? moveHistory.length > 0
-            : moveHistory.length > 1
+    const playerColor = useGameStore((s) => s.playerColor)
 
     const opponentColor =
         playerColor === null ? null : playerColor === "white" ? "b" : "w"
@@ -35,39 +25,7 @@ export default function Page() {
                     <PlayerInfo color={color} />
                 </div>
             </div>
-            <div className="bg-secondary flex-1 w-full landscape:min-w-80 py-5 flex flex-col gap-5 lg:gap-8 justify-center items-center border-l border-black/30 ">
-                <div className="flex flex-col w-full items-center px-2 lg:px-4 gap-1">
-                    <Button
-                        className="cursor-pointer font-semibold max-w-70 w-full "
-                        onClick={undo}
-                        variant={"outline"}
-                        disabled={!canUndo}
-                    >
-                        <Undo />
-                        Undo
-                    </Button>
-                    <Button
-                        className="cursor-pointer font-semibold max-w-70 w-full"
-                        onClick={replay}
-                        variant={"outline"}
-                    >
-                        <RotateCcw />
-                        Replay
-                    </Button>
-                    <Button
-                        asChild
-                        className="cursor-pointer font-semibold max-w-70 w-full"
-                        onClick={resetGame}
-                        variant={"outline"}
-                    >
-                        <Link href="/bot">
-                            <Plus />
-                            New Game
-                        </Link>
-                    </Button>
-                </div>
-                <HistoryController />
-            </div>
+            <BotControls />
         </div>
     )
 }
---
+++ apps/web/src/features/bot/components/bot-controls.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@/components/ui/button"
+import { useBotStore } from "@/features/bot/store"
+import HistoryController from "@/features/game/components/history-controller"
+import { useGameStore } from "@/features/game/game-store"
+import { Plus, RotateCcw, Undo } from "lucide-react"
+import Link from "next/link"
+
+export default function BotControls() {
+    const playerColor = useGameStore((s) => s.playerColor)
+    const moveHistory = useGameStore((s) => s.moveHistory)
+    const undo = useGameStore((s) => s.undo)
+    const resetGame = useGameStore((s) => s.resetGame)
+    const replay = useBotStore((s) => s.replayBotGame)
+
+    const canUndo =
+        playerColor === "white"
+            ? moveHistory.length > 0
+            : moveHistory.length > 1
+
+    return (
+        <div className="bg-secondary flex-1 w-full landscape:min-w-80 py-5 flex flex-col gap-5 lg:gap-8 justify-center items-center border-l border-black/30 ">
+            <div className="flex flex-col w-full items-center px-2 lg:px-4 gap-1">
+                <Button
+                    className="cursor-pointer font-semibold max-w-70 w-full "
+                    onClick={undo}
+                    variant={"outline"}
+                    disabled={!canUndo}
+                >
+                    <Undo />
+                    Undo
+                </Button>
+                <Button
+                    className="cursor-pointer font-semibold max-w-70 w-full"
+                    onClick={replay}
+                    variant={"outline"}
+                >
+                    <RotateCcw />
+                    Replay
+                </Button>
+                <Button
+                    asChild
+                    className="cursor-pointer font-semibold max-w-70 w-full"
+                    onClick={resetGame}
+                    variant={"outline"}
+                >
+                    <Link href="/bot">
+                        <Plus />
+                        New Game
+                    </Link>
+                </Button>
+            </div>
+            <HistoryController />
+        </div>
+    )
+}
