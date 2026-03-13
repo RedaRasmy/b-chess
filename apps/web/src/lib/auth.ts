8b66ceb677fcfb7554d53205784c302dd2616453
reda <red7mer@gmail.com>
2026-03-13 22:23:41 +0000

    refactor: move db to a separate package

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
-import { db } from "@/db"
+import { db } from "@bchess/db"
 import { username } from "better-auth/plugins"
 
 export const auth = betterAuth({
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-    out: "./src/db/migrations",
-    schema: "./src/db/schemas/index.ts",
+    out: "./migrations",
+    schema: "./src/schemas/index.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
