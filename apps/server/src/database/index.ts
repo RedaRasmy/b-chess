70165fab580e844a36e9478a09419f7e9e3fbf46
reda <red7mer@gmail.com>
2026-07-25 03:47:25 +0100

    fix: update import path

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from './schemas';
+import * as schema from './schema';
 
 const client = postgres(process.env.DATABASE_URL!, {
   max: 1,
