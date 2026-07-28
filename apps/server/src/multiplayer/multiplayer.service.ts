012bf852f7aad236fbee592f496c2d3a22dcb043
reda <red7mer@gmail.com>
2026-07-28 19:38:28 +0100

    fix: prevent playing against yourself

--- apps/server/src/multiplayer/multiplayer.service.ts
+++ apps/server/src/multiplayer/multiplayer.service.ts
@@ -3,7 +3,7 @@ import { CreateGameDto } from './dto/create-game.dto';
 import { DATABASE_CONNECTION } from '../database/database.module';
 import { type Database } from '@bchess/db';
 import { games } from '@bchess/db/tables';
-import { and, eq, inArray, or, sql } from 'drizzle-orm';
+import { and, eq, inArray, ne, or } from 'drizzle-orm';
 import {
   OngoingGame,
   OngoingGameWithPlayers,
@@ -15,12 +15,16 @@ export class MultiplayerService {
   constructor(@Inject(DATABASE_CONNECTION) private readonly db: Database) {}
 
   async findOrCreateMatch(dto: CreateGameDto, userId: string) {
-    const matching = await this.db.query.games.findFirst({
+    const match = await this.db.query.games.findFirst({
       where: (games) =>
-        and(eq(games.status, 'matching'), eq(games.timer, dto.timer)),
+        and(
+          eq(games.status, 'matching'),
+          eq(games.timer, dto.timer),
+          ne(games.whiteId, userId),
+        ),
     });
 
-    if (!matching) {
+    if (!match) {
       const { base } = parseTimerOption(dto.timer);
       const [newGame] = await this.db
         .insert(games)
@@ -37,13 +41,13 @@ export class MultiplayerService {
     const [game] = await this.db
       .update(games)
       .set({ status: 'preparing', blackId: userId })
-      .where(eq(games.id, matching.id))
+      .where(eq(games.id, match.id))
       .returning();
 
     return {
       status: 'MATCH_FOUND',
       game,
-      players: [matching.whiteId, userId],
+      players: [match.whiteId, userId],
     } as const;
   }
 
