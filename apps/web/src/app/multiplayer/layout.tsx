36c926270beb1b753f8e5e97a59efb00f6ecf36c
reda <red7mer@gmail.com>
2026-08-03 23:44:43 +0100

    feat: show sign-in required notification

--- apps/web/src/app/multiplayer/layout.tsx
+++ apps/web/src/app/multiplayer/layout.tsx
@@ -3,6 +3,7 @@ import LoadingPage from "@/components/loading-page"
 import { authClient } from "@/lib/auth-client"
 import { useRouter } from "next/navigation"
 import { ReactNode, useEffect } from "react"
+import { toast } from "sonner"
 
 export default function Layout({ children }: { children: ReactNode }) {
     const { data: session, isPending } = authClient.useSession()
@@ -10,6 +11,9 @@ export default function Layout({ children }: { children: ReactNode }) {
 
     useEffect(() => {
         if (!isPending && !session) {
+            toast.error("Sign-in to use multiplayer feature!", {
+                richColors: true,
+            })
             router.replace("/auth/login")
         }
 
